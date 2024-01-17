3a9b10c69ddba2b4f9c08171afe2c6b3258f8dcf
Myiwen <106417750+yiwenmo@users.noreply.github.com>
2024-01-17 16:40:19 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-docker build -t mo_gradcam:test0103 .
-# docker run -it --rm --gpus=all --shm-size="64g" --name test mo_gradcam:test0102
-docker run -it --gpus=all --shm-size="64g" --name test_demo mo_gradcam:test0103
+docker build -t gradcam image
+docker run -it --rm --gpus=all --shm-size="64g" -p 3000:3000 --name test_demo gradcam
