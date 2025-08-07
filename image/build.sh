4e357fad2e6d817c524271d5ab7b9c593d054733
Myiwen <winniemyiwen@gmail.com>
2025-08-07 11:07:40 +0800

    exclude model files

--- image/build.sh
+++ image/build.sh
@@ -1,4 +1,5 @@
-docker build -t gradcam image
+#!/bin/bash
+docker build -t gradcam .
 
 # docker run -it --rm --gpus=all --shm-size="64g" -p 3000:3000 --name demo gradcam # use GPU
 docker run -it --rm -p 3000:3000 --name demo gradcam # use CPU
